20e62f9117f16620b871a39584abde22c2ceae1a
qooqqoor <qooqqoor123@gmail.com>
2021-03-24 16:26:56 +0800

    通知

--- src/page/notification.js
+++ src/page/notification.js
@@ -1,14 +1,44 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import '../css/notification.css'
 import { link } from '../com/tool'
-
+import img from "../static/img/notification/2.jpg"
 const Notification = ()=>{
 
   const [text,setText] = useState("")
   const [onLoad,setonLoad] = useState(false)
+  const [title,setTitle] = useState("來測試看看吧！")
+  const [second,setSecond] = useState("")
+  const [requestPermission,setRequestPermission] = useState(false)
 
   const sendNotification = ()=>{
-    console.log(text)
+    if (!('Notification' in window)) {
+      setTitle('你的瀏覽器不支援呦！');
+      return
+    }
+    if (Notification.permission === 'default' || Notification.permission === undefined) {
+      window.Notification.requestPermission((permission)=>{
+        switch (permission) {
+          case "granted":
+            setSecond("你已開啟新世界的大門!")
+            const notifyConfig = {
+              body : text,
+              icon : img
+            }
+            const notification = new window.Notification('我在這兒~', notifyConfig);
+            notification.onclick=(e)=>{
+              e.preventDefault()
+              window.open('https://cythilya.github.io/2017/07/09/notification/')
+            }
+            setTimeout(()=>{notification.close()},5000)
+            break
+          case "denied":
+            setSecond("你拒絕了我，我好傷心吶 ～")
+            return
+            break
+        }
+      })
+    }
+
 
   }
 
@@ -26,7 +56,8 @@ const Notification = ()=>{
       </div>
 
       <div className='text_box'>
-        <h3>來測試看看吧！</h3>
+        <h3>{title}</h3>
+        <h4>{second}</h4>
         <textarea type="text"
                   onChange={(e)=>setText(e.target.value)}
                   rows={5}
